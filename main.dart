a2f2a1ef40dbc10153ef9803a0cee7d0b717591b
sky2000sh <sky2000sh@gmail.com>
2024-12-31 17:05:33 +0900

    complete a course on dart_tour with nomadcoders

--- main.dart
+++ main.dart
@@ -113,10 +113,59 @@
 // }
 
 
+class Human {
+  final String name;
+  Human({required this.name});
+  void sayHello() {
+    print("Hi, my name is $name");
+  }
+}
+
+class Strong {
+  final double strenghtLevel = 1500.99;
+}
+
+class QuickRunner {
+  void runQuick() {
+    print("runnnnn!");
+  }
+}
+
+class Tall {
+  final double height = 1.99;
+}
+
+enum Team {blue, red}
+
+// class Player extends Human {
+//   final Team team;
+
+//   Player({
+//     required this.team,
+//     required String name,
+//   }) : super(name : name);
+
+//   @override
+//   void sayHello() {
+//     super.sayHello();
+//     print('and I play for ${team}');
+//   }
+// }
+
+// Mixins
+// class Player with Strong, QuickRunner, Tall {
+//   final Team team;
+// }
+
+// class Horse with Strong, QuickRunner {}
+
 
 void main() {
 
-  // 241230 테스트용 이 행 삭제할 것
+
+
+  //var player = Player(team: Team.red, name : 'sihyeon');
+  
 
   // print('Hello World');
   // var name = 'sihyeon';
